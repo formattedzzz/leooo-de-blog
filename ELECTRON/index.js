f884be0fc7cfacdbb27c37845bb8c238e40b3efb
Fulin Liu <formattedzzz@gmail.com>
2019-10-23 10:53:58 +0800

    push

--- ELECTRON/index.js
+++ ELECTRON/index.js
@@ -1,19 +1,40 @@
-const {app, BrowserWindow} = require('electron')
-
+const {app, BrowserWindow, ipcMain, Tray, Menu, nativeImage} = require('electron')
+let path = require('path')
 let window = null
+let tray = null
 
+ipcMain.on('something', (event, data) => {
+  console.log(event, data)
+  event.sender.send('something1', '我是主进程返回的值')
+})
+leooo = {name: 'chenlei'}
 function createWindow () {
   window = new BrowserWindow({
     width: 1280,
-    height: 720
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: true,
+      allowRunningInsecureContent: false
+    }
   })
   window.loadFile('./index.html')
+  // window.loadFile('https://ecs2.nncz.cn/static/html/tms-help.html')
   window.webContents.openDevTools()
   window.on('closed', () => {
     console.log('window closed')
     window = null
   })
+  const trayIcon = nativeImage.createFromPath(path.resolve(__dirname, './tray.png')).resize({width: 20, height: 18})
+  tray = new Tray(trayIcon)
+  const contextMenu = Menu.buildFromTemplate([
+    {label: '查看消息', type: 'radio', click: () => {}},
+    {label: '删除消息', type: 'radio', click: () => {}},
+  ])
+  tray.setToolTip('leooo de electron application')
+  tray.setContextMenu(contextMenu)
 }
+
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
